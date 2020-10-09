43bdb41f4a1d78888a3a1af0be638f4300e986e8
unknown <mariechou2001@gmail.com>
2020-10-09 10:45:32 +0100

    second commit

--- entrée_matériel.php
+++ entrée_matériel.php
@@ -199,7 +199,7 @@
                         <svg width="2em" height="1.6em" viewBox="0 0 16 16" class="bi bi-people-fill" fill="white" xmlns="http://www.w3.org/2000/svg">
                           <path fill-rule="evenodd" d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1H7zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm-5.784 6A2.238 2.238 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.325 6.325 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1h4.216zM4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5z"/>
                           </svg>
-                          <p>Fournisseurs</p>
+                          <p>Matériels</p>
                         </a>
                       </li>
                       <li class="nav-item">
@@ -337,13 +337,8 @@
                       <?php
                       }
                      ?>
-                    
-                     
-                     
-                    </tbody>
-                   
-                   
-                  </table>
+                      </tbody>
+                    </table>
                   <?php
                 }else{
                     echo "<h3>No records found. Please insert some records</h3>";
